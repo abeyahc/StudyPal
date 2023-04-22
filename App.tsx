da17c609e9902011a6e79cb4a64a7c27d2047702
Abeyah Calpatura <abeyahc@Abeyahs-MacBook-Air.local>
2023-04-21 23:36:25 -0700

    initial commit.

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import {Text, View, StyleSheet} from 'react-native';
+ 
+ const YourApp = () => {
+   return (
+     <View style={styles.general}>
+       <Text style={styles.baseText}>Hello World</Text>
+     </View>
+   );
+ };
+
+ const styles = StyleSheet.create({
+    general: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    baseText: {
+      fontSize: 25, 
+    }
+ })
+ 
+
+export default YourApp;
