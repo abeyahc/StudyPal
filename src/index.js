3b9dcffcb8089c64fcfb9a8c8e4c80e72091ef99
Christian Kinyon <67552722+Cjkinyon@users.noreply.github.com>
2023-04-22 18:34:29 -0700

    fancy danc

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import About from './pages/About';
-import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const client = new ApolloClient({
     uri: 'http://localhost:8080/v1/graphql',
